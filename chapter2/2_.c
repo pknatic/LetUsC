db0a59981faffa0d008bc58ef7ee9e99cb440e97
Pushpendra Kumar <INFO.PKRAJPUT@GMAIL.COM>
2022-11-13 21:33:22 +0530

    Update 2_.c
    If the length of three sides of a triangle are input through the keyboard, write a program to find the area of the triangle.

--- chapter2/2_.c
+++ chapter2/2_.c
@@ -1,1 +1,13 @@
-
+/*If the length of three sides of a triangle are input through the keyboard, write a program to find the area of the triangle.*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,sp,area;
+    printf("\n Enter sides of a triangle : ");
+    scanf("%f%f%f",&a,&b,&c);
+    sp=(a+b+c)/2;
+    area=sqrt(sp*(sp-a)*(sp-b)*(sp- c));
+    printf("Area of triangle = %f\n",area);
+    return 0;
+}
