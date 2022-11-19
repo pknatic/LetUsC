6bf917e55c681d123d58d8c4a0b7351fe056ee35
Pushpendra Kumar <INFO.PKRAJPUT@GMAIL.COM>
2022-11-19 12:13:49 +0530

    Update and rename chapter1/F_c.c to Chapter_1/F_c.c
    Paper of size A0 has dimensions 1189mm x 841mm. Each subsequent size A(n) is defined as A(n-1) cut in half, parallel to its shorter sides. Thus, Paper A1 would have dimensions 841mm x 594mm. Write a program to calculate and print paper sizes A0, A1......A8.

--- Chapter_1/F_c.c
+++ Chapter_1/F_c.c
@@ -1,4 +1,4 @@
-/*Paper of size A0 has dimension 1189mm x 841mm. Each subsequent size A(n) is defined as A(n-1) cut in half, parallel to its shorter sides. Thus, Paper of A1 would have dimension 841mm x 594mm. Write a program to calculate and print paper sizes A0,A1......A8.*/
+/*Paper of size A0 has dimensions 1189mm x 841mm. Each subsequent size A(n) is defined as A(n-1) cut in half, parallel to its shorter sides. Thus, Paper A1 would have dimensions 841mm x 594mm. Write a program to calculate and print paper sizes A0, A1......A8.*/
 #include <stdio.h>
 #include <math.h>
 int main()
