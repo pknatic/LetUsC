41d82baa32a5545a44776f40cfc6d81da4eeddb5
Pushpendra Kumar <info.pkrajput@gmail.com>
2022-11-22 23:03:18 +0530

    Create 3_____.c
    Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not.

---
+++ Chapter_3/3_____.c
@@ -0,0 +1,23 @@
+/*Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not.*/
+#include <stdio.h>
+int main()
+{
+int yr;
+printf("Enter Year = ");
+scanf("%d",&yr);
+if(yr%100==0)
+{
+    if(yr%400==0)
+    printf("Leap Year\n");
+    else
+    printf("Not a leap year\n");   
+}
+else
+{
+    if(yr%4==0)
+    printf("Leap Year\n");
+    else
+    printf("Not a leap year");
+}
+    return 0;
+}
