ed578a5c2d4ba15b82072f4eb96e1d1955f14a46
Pushpendra Kumar <info.pkrajput@gmail.com>
2022-11-29 21:43:29 +0530

    Update 4_.c
    Write a program to calculate the division obtained by the student.

--- Chapter_4/4_.c
+++ Chapter_4/4_.c
@@ -1,1 +1,31 @@
+/*Write a program to calculate the division obtained by the student.*/
+#include<stdio.h>
+int main()
+{
+    int m1,m2,m3,m4,m5,per;
+    printf("Enter marks in five subjects \n");
+    scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
+    per=(m1+m2+m3+m4+m5)*100/500;
+    printf("Percentages of student = %d\n",per);
+    if (per>60)
+    {
+        printf("First Devision\n");
+    }
+    else
+    {
+        if(per>=50)
+        printf("Second Devision");
+        else 
+        {
+            if(per>=40)
+            printf("Third Devision");
+            else
+            {
+                if(per<40);
+                printf("Fail");
+            }
 
+        }
+    }
+    return 0;
+}
