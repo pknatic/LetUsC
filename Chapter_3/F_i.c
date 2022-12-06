99b21affc3d4999e55939c0257f65f18ca082dd7
Pushpendra Kumar <info.pkrajput@gmail.com>
2022-12-06 09:48:11 +0530

    Update F_i.c
    According to the Gregorian calendar, it was Monday on the date 01/01/01. If any year is input through the keyboard write a program to find out the day on 1st January of this year.

--- Chapter_3/F_i.c
+++ Chapter_3/F_i.c
@@ -1,45 +1,41 @@
 /* According to the Gregorian calendar, it was Monday on the date 01/01/01. If any year is input through the keyboard write a program to find out the day on 1st January of this year. */
-<stdio.h>
+#include<stdio.h>
 int main()
 {
-int yr,day,normal_days,leap_days;
+int year,days,normal_days,leap_days;
 printf("Enter the year = ");
-scanf("%d",&yr);
-normal_days=(yr-1)*365;/*we use -1 because entered year is not completed their 365 days*/
-printf("%d\n",normal_days);
-leap_days=((yr-1)/4+(yr-1)/400)-(yr-1)/100;/*As per rule which year fully devided by 4 and 400 is a leap year and which not devide is not a leap year*/
-printf("%d\n",leap_days);
-day=normal_days+leap_days;/* we enterd +1 because we want to the day of 01/01/YEAR.*/
-printf("%d\n",day);
-day=day%7; /*We need Remainder For Identify the day*/
-printf("%d\n",day);
-if (day==0)
+scanf("%d",&year);
+normal_days=(year-1)*365;//all normal days
+leap_days=(year/4)+(year/400)-(year/100);/* Which years devided by 400 and 4 are leap years but which years devided by 100 are not leap years. */
+days=normal_days+leap_days; //total days 
+days=days%7; /*We need Remainder For Identify the day*/
+if (days==0)
     {
-        printf("The fisrt day of the year %d = Monday ",yr);
+        printf("The fisrt day of the year %d = Monday ",year);
     }
-if (day==1)
+if (days==1)
     {
-        printf("The fisrt day of the year %d = Tuesday ",yr);
+        printf("The fisrt day of the year %d = Tuesday ",year);
     }
-if (day==2)
+if (days==2)
     {
-        printf("The fisrt day of the year %d = Wednesday ",yr);
+        printf("The fisrt day of the year %d = Wednesday ",year);
     }
-if (day==3)
+if (days==3)
     {
-        printf("The fisrt day of the year %d = Thursday ",yr);
+        printf("The fisrt day of the year %d = Thursday ",year);
     }
-if (day==4)
+if (days==4)
     {
-        printf("The fisrt day of the year %d = Friday ",yr);
+        printf("The fisrt day of the year %d = Friday ",year);
     }
-if (day==5)
+if (days==5)
     {
-        printf("The fisrt day of the year %d = Saturday ",yr);
+        printf("The fisrt day of the year %d = Saturday ",year);
     }
-if (day==6)
+if (days==6)
     {
-        printf("The fisrt day of the year %d = Sunday ",yr);
+        printf("The fisrt day of the year %d = Sunday ",year);
     }
     return 0;
 }
