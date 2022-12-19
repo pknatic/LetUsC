3ce21ecf3001fa721cf933e7b7f7b333684c2a5f
Pushpendra Kumar <info.pkrajput@gmail.com>
2022-12-19 22:41:14 +0530

    Update 4_E_b.c

--- Chapter_4/4_E_b.c
+++ Chapter_4/4_E_b.c
@@ -6,5 +6,6 @@ int year;
 printf("Enter the year = ");
 scanf("%d",&year);
 (year%400==0)||(year%4==0)&&(year%100!=0) ? printf("Leap year."):printf("Not a leap year.");
+// year%100==0?(year%400==0?printf("Leap year"):printf("Not a leap year")):(year%4==0?printf("Leap year"):printf("Not a leap year"));
     return 0;
 }
